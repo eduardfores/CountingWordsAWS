3e03b5ac33d50dac7ceccbe0516ef6a421541cb5
Denys Sydorenko <denys.sydorenko1997@gmail.com>
2018-05-17 00:18:36 +0200

    comentarios y arreglos

--- SD_Practica2.py
+++ SD_Practica2.py
@@ -1,3 +1,6 @@
+# Eduard Fores Ferrer, Denys Sydorenko
+# 17/05/2018 v1.0
+
 #from concurrent.futures import ThreadPoolExecutor
 
 import boto3
@@ -7,6 +10,7 @@ import json
 import os
 import time
 
+''' Definicion de variables globales. '''
 ACCESS_KEY='AKIAIOIOCNMRWM34L5ZQ'
 SECRET_KEY='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'
 BUCKET_NAME='putamierdadedocumentaciones'
@@ -24,21 +28,24 @@ if n < 1:
 	print "\nNo pueden haber menos de 1 mappers.\n"
 	sys.exit(0)
 
-txt=""
-
+''' Creamos el Path en el que haremos funcionar nuestro programa. '''
 os.system("mkdir -p "+PATH)
 
-"maxim 6291456"
+''' Ponemos el nombre del fichero en el key1. '''
 payload3=dict()
 payload3["key1"]=FILE
 
+''' Definiciones de ACCESS_KEY y SECRET_KEY para poder acceder desde fuera de Amazon. '''
+''' Diferencia entre resource y client, es que uno es de mas alto nivel que el otro. '''
 s3=boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
 s3_r=boto3.resource('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
 
-#Subida
+''' Para subir un archivo a nuestro BUCKET de Amazon. '''
 #s3.upload_file(FILE, BUCKET_NAME, FILE)
-#Descarga
+
+''' Para descargar un archivo de nuestro BUCKET de Amazon. '''
 '''
+txt=""
 try:
 	s3.download_file(BUCKET_NAME, FILE, PATH+FILE)
 except botocore.exceptions.ClientError as e:
@@ -47,16 +54,18 @@ except botocore.exceptions.ClientError as e:
 	else:
 		raise
 
-
 with open(PATH+FILE, 'r') as reader:
 	for line in reader:
 		txt+=line
 payload3["key1"]=txt
 '''
 
+''' Definiciones de ACCESS_KEY y SECRET_KEY para poder acceder desde fuera de Amazon. '''
 client = boto3.client('lambda', region_name="eu-west-3", aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
 
+''' Invocamos a los mappers. '''
 for i in range(0,n):
+	''' Cada mapper tendra el fichero que tiene que leer y el identificador de mapper. '''
 	payload3["key2"]=i
 	client.invoke(
 		FunctionName="mapper",
@@ -64,17 +73,22 @@ for i in range(0,n):
 		Payload=json.dumps(payload3)
 	)
 
+''' Ponemos el sleep de 20 segundos porque los mapper tardan unos 15 segundos mas o menos. '''
 time.sleep(20)
 
+''' Definimos el payload3 que utilizaremos a continuacion. '''
 payload3={
-	"Key1":n
+	"key1":n
 }
 
+''' Invocamos al reducer. '''
 response=client.invoke(
 	FunctionName="reducer",
 	InvocationType='RequestResponse',
 	Payload=json.dumps(payload3)
 )
+
+''' Devuelve por pantalla el map_final y el numero total de palabras. '''
 print response['Payload'].read()
 
 
